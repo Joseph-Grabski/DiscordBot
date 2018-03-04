2e8c4e96df43b6ccdc000e7107472ea78874cc66
TheCookieMonster31 <biscuitgamer31@gmail.com>
2018-03-04 11:50:37 +0000

    Added to github

---
+++ DiscordBot/Modules/Memes/Meme.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+using Discord.WebSocket;
+
+namespace DiscordBot.Modules
+{
+    public class Meme : ModuleBase<SocketCommandContext>
+    {
+
+        //Context.User;
+        //Context.Client;
+        //Context.Guild;
+        //Context.Message;
+        //Context.Channel;
+
+        //EmbedBuilder builder = new EmbedBuilder();
+        //builder.AddInlineField("Hello there!", Context.User.ToString())
+        //    .WithColor(Color.Green);
+
+        //await ReplyAsync("", false, builder.Build());
+
+        
+
+        [Command("noob")]
+        public async Task MessageAsync(string name = "")
+        {
+            if (name == "")
+            {
+                await ReplyAsync($"{Context.User.Mention} is a noob!");
+            }
+            else
+            {
+                await ReplyAsync($"{name} is a noob!");
+            }
+            
+        }
+
+        [Command("slap")]
+        public async Task SlapAsync(SocketGuildUser target, int force = 0)
+        {
+            switch (force)
+            {
+                case 1:
+                    await ReplyAsync($"{Context.User.Mention} lightly slapped {target.Mention} :clap:");
+                    break;
+                case 2:
+                    await ReplyAsync($"{Context.User.Mention} slapped {target.Mention} with vigour! :clap:");
+                    break;
+                case 3:
+                    await ReplyAsync($"{Context.User.Mention} falcon punched {target.Mention}, knocking them out :clap:");
+                    break;
+                default:
+                    await ReplyAsync($"{Context.User.Mention} slapped {target.Mention} :clap:");
+                    break;
+            }
+            
+        }
+
+        [Command("betterbot")]
+        public async Task BotAsync()
+        {
+            await ReplyAsync("Joe's bot is better!");
+        }
+    }
+}
---
+++ DiscordBot/Modules/Misc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+using Discord.WebSocket;
+using System.IO;
+
+namespace DiscordBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("repeat")]
+        public async Task RepeatAsync([Remainder] string content)
+        {
+            await ReplyAsync($"{content}");
+        }
+
+        [Command("choose")]
+        public async Task ChooseAsync([Remainder] string message)
+        {
+            string[] options = message.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Random r = new Random();
+            string selection = options[r.Next(0, options.Length)];
+
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.AddField("I have chosen!", selection)
+                .WithColor(Color.Gold);
+            builder.WithThumbnailUrl("http://www.mrtownsend.com/icons/es.jpg");
+
+            await ReplyAsync("", false, builder.Build());
+        }
+
+        [Command("hi")]
+        public async Task HelloAsync()
+        {
+            await ReplyAsync($"Hi there {Context.User.Mention}!");
+        }
+    }
+}
